409369262968bcc32df75de39f862d8f054e1833
TamonudSharma <73441372+spidey711@users.noreply.github.com>
2022-03-24 08:18:47 +0530

    base features completed

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# Kivy latest version: 2.0.0
-
 # Imports
 try:
     import os
@@ -50,10 +48,9 @@ class RegisterScreen(Screen):
     username = ObjectProperty(None)
     password = ObjectProperty(None)
     
-    # saves data in db
     def register(self):
-       # making sure no details are empty 
     
+       # making sure no details are empty 
         if self.firstname.text and self.lastname.text and self.username.text and self.password.text is not None and self.password.text != "":
             cursor.execute("""INSERT INTO userdata VALUES("{}", "{}", "{}", "{}")""".format(
                 self.firstname.text, 
@@ -88,6 +85,7 @@ class LoginScreen(Screen):
     password = ObjectProperty(None)
    
     def login(self):
+
         verify = (self.username.text, self.password.text)
         cursor.execute("SELECT username, password FROM userdata")
         data = cursor.fetchall()
@@ -116,6 +114,7 @@ class HomeScreen(Screen):
     
     @staticmethod
     def logout():
+
         popup = Popup(
             title="Message", 
             content=Label(text="Logged Out.", color="white"), 
@@ -126,6 +125,7 @@ class HomeScreen(Screen):
 
     @staticmethod
     def exitApp():
+
         exit()
 
 
@@ -134,46 +134,58 @@ class BuyMedicinesScreen(Screen):
     medicine = ObjectProperty(None)
     
     def addToCart(self):
-        if self.medicine.text.strip():
-            li = list(self.medicine.text.split(","))
-            for item in li:
-                medList.append(item.strip()) # remove whitespaces
-            popup = Popup(
-                title="Message",
-                content=Label(text="Med(s) added to cart.", color="white"),
-                size_hint=(None, None),
-                size=(300, 200)
-            )
-            popup.open()
-            if len(medList) != 0: return True
-            else: return False
+
+        if self.medicine.text.isalpha() or "," in self.medicine.text: # no special characters
+            if self.medicine.text.strip(): # no blank input box
+                li = list(self.medicine.text.split(","))
+                for item in li:
+                    item = item.lower().strip() # remove whitespaces
+                    medList.append(item) 
+                popup = Popup(
+                    title="Message",
+                    content=Label(text="Med(s) added to cart.", color="white"),
+                    size_hint=(None, None),
+                    size=(300, 200)
+                )
+                popup.open()
+                return True
+            else:
+                popup = Popup(
+                    title="Message",
+                    content=Label(text="No medicine name was mentioned.", color="white"),
+                    size_hint=(None, None),
+                    size=(300, 200)
+                )
+                popup.open()
+                return False
         else:
             popup = Popup(
                 title="Message",
-                content=Label(text="No medicine name was mentioned.", color="white"),
+                content=Label(text="Following are not allowed:-\nSpecial Characters, Numbers\nexcept commas (,)", color="white"),
                 size_hint=(None, None),
                 size=(300, 200)
             )
             popup.open()
+            return False
 
 
 class CartScreen(Screen):
 
     @staticmethod
     def infoCrocin():
-        return "Uses: \nContents: \nPrice: "
+        return "Uses: Cold, Flu, Headache etc.\nContents: Paracetamol, Caffeine\nPrice: Rs 30\n(1 leaf has 6 tablets)"
 
     @staticmethod
     def infoNexium():
-        return "Uses: \nContents: \nPrice: "
+        return "Uses: Acidity, Gastric Ulcer etc.\nContents: Magnesium, R-Isomers\nPrice: Rs 25\n(1 leaf has 6 tablets)"
 
     @staticmethod
     def infoParacetamol():
-        return "Uses: \nContents: \nPrice: "
+        return "Uses: Acute Pain, Aches etc.\nContents: Purified Talc, Starches\nPrice: Rs 27\n(1 leaf has 6 tablets)"
 
     @staticmethod
     def infoAspirin():
-        return "Uses: \nContents: \nPrices: "
+        return "Uses: Swelling, Cold, Aches\nContents: Cellulose, Triacetin\nPrices: Rs 32\n(1 leaf has 6 tablets)"
 
     def billing(self):    
         cursor.execute("SELECT * FROM prices")
@@ -181,23 +193,17 @@ class CartScreen(Screen):
         priceList = [] # to sum up the price of all meds
         
         with open("bills.txt", "w") as bill:
-            # formatting bill
             bill.write("-------------- BILL RECEIPT --------------\n\n")
             bill.write(f"Date: {datetime.datetime.today().date()}\nTime: {datetime.datetime.today().time()}\n\nMedicines Purchased:-\n\n")
-
-            # compare meds in medlist with meds in table
-            for med in medList:
-                for item in data: # item = (name of med, price)
+            for med in medList:             # compare meds in medlist with meds in table
+                for item in data:           # item = (name of med, price)
                     if item[0] == med:
                         priceList.append(item[1])
                         bill.write("{}:  Rs {}\n".format(str(item[0]).capitalize(), item[1]))
-            
-            # formatting bill pt.2
-            bill.write("\nTotal Price:  Rs {}\n\n".format(sum(priceList)))
-            bill.write("Thank you for your purchase!\n")
-            bill.write("---------------------------------------------\n\n")
+            bill.write("\nTotal Price:  Rs {}\n\nThank you for your purchase!\n".format(sum(priceList)))
+            bill.write("--------------------------------------------\n\n")
 
-        # popups for buying without meds ATC and with meds ATC (ATC - Added To Cart)
+        # popups for purchasing meds with or without adding them to cart
         if len(priceList) > 0:
             popup = Popup(
                 title="Message",
@@ -215,7 +221,6 @@ class CartScreen(Screen):
                 size=(300, 200) 
             )
             popup.open()
-        
         # reset variables
         medList.clear()
         priceList.clear()
